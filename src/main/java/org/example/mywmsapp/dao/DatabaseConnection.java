3552cfe8df8e3546ca389e82efc4b6142df0347a
Khalil <khalil@Khalils-2.local>
2025-04-09 00:23:04 +0100

    i

--- src/main/java/org/example/mywmsapp/dao/DatabaseConnection.java
+++ src/main/java/org/example/mywmsapp/dao/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/mywmsdb";
     private static final String USER = "root";
-    private static final String PASSWORD = "Khalil@2001";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         try {
